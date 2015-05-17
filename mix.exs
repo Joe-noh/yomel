d9bffeabaa2f265cb8e6fcf73bee79022a461cfd
Joe-noh <goflb.jh@gmail.com>
2015-05-18 08:46:55 +0900

    fill mix.exs

--- mix.exs
+++ mix.exs
@@ -34,29 +34,30 @@ defmodule Yomel.Mixfile do
     [app: :yomel,
      version: "0.2.0",
      elixir: "~> 1.0",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      compilers: [:nif | Mix.compilers],
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
-  defp deps do
-    []
+  defp deps, do: []
+
+  defp description do
+    "Decodes yaml into elixir terms"
+  end
+
+  defp package do
+    [files: ["lib", "priv", "c_src", "mix.exs", "README*", "LICENSE*"],
+     contributors: ["Joe Honzawa"],
+     licenses: ["MIT"],
+     links: %{
+       "GitHub" => "https://github.com/Joe-noh/yomel"
+     }]
   end
 end
