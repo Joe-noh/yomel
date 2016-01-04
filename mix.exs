f447c547f11e6c8d84201a386713ff98df9ca66c
Tom von Schwerdtner <tvon@covermymeds.com>
2016-01-03 21:13:42 -0500

    Honor CFLAGS and LDFLAGS in build process

--- mix.exs
+++ mix.exs
@@ -3,29 +3,15 @@ defmodule Mix.Tasks.Compile.Nif do
 
   @shortdoc "compile C source"
 
-  @compiler "clang"
-  @erl_flag "-I#{:code.root_dir}/erts-#{:erlang.system_info :version}/include"
-  @c_files  [__DIR__, "c_src", "*.c"] |> Path.join |> Path.wildcard
-  @out_opt  "-o #{Path.join [__DIR__, "priv", "yomel.so"]}"
-
   def run(_) do
-    [__DIR__, "priv"]
-    |> Path.join
-    |> File.mkdir_p!
-
-    [@compiler, @erl_flag, @c_files, shared_opts, @out_opt]
-    |> List.flatten
-    |> Enum.join(" ")
-    |> Mix.shell.cmd
-  end
-
-  defp shared_opts, do: ["-shared" | os_shared_opts]
-
-  defp os_shared_opts do
-    case :os.type do
-      {:unix, :darwin} -> ~w(-dynamiclib -undefined dynamic_lookup -lyaml)
-      _other -> []
+    if match? {:win32, _}, :os.type do
+      raise RuntimeError, message: "Windows is not currently supported"
+    else
+      {result, _error_code} = System.cmd("make", ["priv/yomel.so"], stderr_to_stdout: true)
+      IO.binwrite result
     end
+    
+    :ok
   end
 end
 
