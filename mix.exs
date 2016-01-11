602083a27890cf958eaaa93edad4bfef4c3ee967
Joe-noh <goflb.jh@gmail.com>
2016-01-11 23:33:37 +0900

    Revert "Merge pull request #13 from tvon/local-libyaml"
    This reverts commit 48dc73d29e50fc5dd7d16707ad799ea51a14c5e4, reversing
    changes made to e8e36dd11fca2ee5779bbe97049f0746f964589d.

--- mix.exs
+++ mix.exs
@@ -1,14 +1,30 @@
-defmodule Mix.Tasks.Compile.Yaml do
-  @shortdoc "Compiles libyaml"
+defmodule Mix.Tasks.Compile.Nif do
+  use Mix.Task
+
+  @shortdoc "compile C source"
+
+  @compiler "clang"
+  @erl_flag "-I#{:code.root_dir}/erts-#{:erlang.system_info :version}/include"
+  @c_files  [__DIR__, "c_src", "*.c"] |> Path.join |> Path.wildcard
+  @out_opt  "-o #{Path.join [__DIR__, "priv", "yomel.so"]}"
 
   def run(_) do
-    # TODO: Totally untested
-    if match? {:win32, _}, :os.type do
-      raise RuntimeError, message: "Compiling on windows is not currently supported"
-    else
-      {result, _error_code} = System.cmd("make", ["priv/yomel.so"], stderr_to_stdout: true)
-      IO.binwrite result
-      :ok
+    [__DIR__, "priv"]
+    |> Path.join
+    |> File.mkdir_p!
+
+    [@compiler, @erl_flag, @c_files, shared_opts, @out_opt]
+    |> List.flatten
+    |> Enum.join(" ")
+    |> Mix.shell.cmd
+  end
+
+  defp shared_opts, do: ["-shared" | os_shared_opts]
+
+  defp os_shared_opts do
+    case :os.type do
+      {:unix, :darwin} -> ~w(-dynamiclib -undefined dynamic_lookup -lyaml)
+      _other -> []
     end
   end
 end
@@ -19,12 +35,12 @@ defmodule Yomel.Mixfile do
   def project do
     [app: :yomel,
      version: "0.2.2",
-     elixir: "~> 1.2.0",
+     elixir: "~> 1.0",
      description: description,
      package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     compilers: [:yaml, :elixir, :app],
+     compilers: [:nif | Mix.compilers],
      deps: deps,
      docs: docs]
   end
@@ -35,8 +51,7 @@ defmodule Yomel.Mixfile do
 
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
-     {:ex_doc,  "~> 0.7", only: :dev},
-     {:yaml, git: "git@github.com:yaml/libyaml.git", tag: "0.1.4", app: false, compile: "./bootstrap && ./configure"}]
+     {:ex_doc,  "~> 0.7", only: :dev}]
   end
 
   defp description do
